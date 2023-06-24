ee88ea45abaa1fa0347cc325ec0d68fbf64687ee
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-06-24 18:02:12 +0200

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,135 @@
-import ContactDetailsPage from "pages/ContactDetailsPage";
-import ContactsPage from "pages/ContactsPage";
+// import ContactDetailsPage from "pages/ContactDetailsPage";
+// import ContactsPage from "pages/ContactsPage";
 // import Home from "pages/HomePage";
-import InfoPage from "pages/InfoPage";
-import LoginPage from "pages/LoginPage";
-import RegisterPage from "pages/RegisterPage";
-import {  NavLink, Route, Routes } from "react-router-dom";
-import { Layout } from "./Layout/Layout";
-import HomePage from "pages/HomePage";
+// import InfoPage from "pages/InfoPage";
+// import LoginPage from "pages/LoginPage";
+// import RegisterPage from "pages/RegisterPage";
+// import {  NavLink, Route, Routes } from "react-router-dom";
+// import { Layout } from "./Layout/Layout";
+// import HomePage from "pages/HomePage";
 
-export const App = () => {
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//         margin: '30px',
+//       }}
+//     >
+// <nav>
+
+
+//         <NavLink to="/" end style={{ marginRight: '10px' }}>
+          
+//         </NavLink>
+//         <NavLink to="/register" style={{ marginRight: '10px' }}>
+         
+//         </NavLink>
+//         <NavLink to="/login" style={{ marginRight: '10px' }}>
+       
+//         </NavLink>
+//         <NavLink to="/contacts" style={{ marginRight: '10px' }}>
+        
+//         </NavLink>
+//         <NavLink to="/info" style={{ marginRight: '10px' }}>
+       
+//         </NavLink>
+//       </nav>
+
+//      <Routes>
+ 
+//      <Route path="/" element={<Layout />}>
+//         <Route index element={<HomePage/>} />
+       
+//         <Route path="contacts" element={<ContactsPage />} />
+//         <Route path="contacts/:id" element={<ContactDetailsPage />} />
+//         <Route path="info" element={<InfoPage />} />
+//         {/* <Route path="*" element={<NotFound />} /> */}
+//         <Route path="register" element={<RegisterPage />} />
+//         <Route path="login" element={<LoginPage />} />
+
+//           </Route>
+        
+//       </Routes>
+//     </div>
+//   );
+// };
+// import React, { lazy, Suspense } from 'react';
+// import { NavLink, Route, Routes } from 'react-router-dom';
+// import { Layout } from './Layout/Layout';
+
+// const HomePage = lazy(() => import('../pages/HomePage'));
+// const ContactsPage = lazy(() => import('../pages/ContactsPage'));
+// const ContactDetailsPage = lazy(() => import('../pages/ContactDetailsPage'));
+// const InfoPage = lazy(() => import('../pages/InfoPage'));
+// const RegisterPage = lazy(() => import('../pages/RegisterPage'));
+// const LoginPage = lazy(() => import('../pages/LoginPage'));
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//         margin: '30px',
+//       }}
+//     >
+//       <nav>
+//         <NavLink to="/" end style={{ marginRight: '10px' }}>
+//           Home
+//         </NavLink>
+//         <NavLink to="/register" style={{ marginRight: '10px' }}>
+//           Register
+//         </NavLink>
+//         <NavLink to="/login" style={{ marginRight: '10px' }}>
+//           Login
+//         </NavLink>
+//         <NavLink to="/contacts" style={{ marginRight: '10px' }}>
+//           Contacts
+//         </NavLink>
+//         <NavLink to="/info" style={{ marginRight: '10px' }}>
+//           Info
+//         </NavLink>
+//       </nav>
+
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<HomePage />} />
+//             <Route path="contacts" element={<ContactsPage />} />
+//             <Route path="contacts/:id" element={<ContactDetailsPage />} />
+//             <Route path="info" element={<InfoPage />} />
+//             <Route path="register" element={<RegisterPage />} />
+//             <Route path="login" element={<LoginPage />} />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+//     </div>
+//   );
+// };
+import React, { lazy, Suspense } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const ContactsPage = lazy(() => import('pages/ContactsPage'));
+const ContactDetailsPage = lazy(() => import('pages/ContactDetailsPage'));
+const InfoPage = lazy(() => import('pages/InfoPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+
+const App = () => {
   return (
     <div
       style={{
@@ -21,39 +142,26 @@ export const App = () => {
         margin: '30px',
       }}
     >
-<nav>
-        <NavLink to="/" end style={{ marginRight: '10px' }}>
-          
-        </NavLink>
-        <NavLink to="/register" style={{ marginRight: '10px' }}>
-         
-        </NavLink>
-        <NavLink to="/login" style={{ marginRight: '10px' }}>
-       
-        </NavLink>
-        <NavLink to="/contacts" style={{ marginRight: '10px' }}>
-        
-        </NavLink>
-        <NavLink to="/info" style={{ marginRight: '10px' }}>
-       
-        </NavLink>
+      <nav>
+        <NavLink to="/" end style={{ marginRight: '10px' }}></NavLink>
+        <NavLink to="/register" style={{ marginRight: '10px' }}></NavLink>
+        <NavLink to="/login" style={{ marginRight: '10px' }}></NavLink>
+        <NavLink to="/contacts" style={{ marginRight: '10px' }}></NavLink>
+        <NavLink to="/info" style={{ marginRight: '10px' }}></NavLink>
       </nav>
 
-     <Routes>
- 
-     <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage/>} />
-       
-        <Route path="contacts" element={<ContactsPage />} />
-        <Route path="contacts/:id" element={<ContactDetailsPage />} />
-        <Route path="info" element={<InfoPage />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} />
-
-          </Route>
-        
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Suspense fallback={<div>Loading...</div>}><HomePage /></Suspense>} />
+          <Route path="contacts" element={<Suspense fallback={<div>Loading...</div>}><ContactsPage /></Suspense>} />
+          <Route path="contacts/:id" element={<Suspense fallback={<div>Loading...</div>}><ContactDetailsPage /></Suspense>} />
+          <Route path="info" element={<Suspense fallback={<div>Loading...</div>}><InfoPage /></Suspense>} />
+          <Route path="register" element={<Suspense fallback={<div>Loading...</div>}><RegisterPage /></Suspense>} />
+          <Route path="login" element={<Suspense fallback={<div>Loading...</div>}><LoginPage /></Suspense>} />
+        </Route>
       </Routes>
     </div>
   );
 };
+
+export default App;
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,64 +1,22 @@
-// import React from 'react';
-// // import { useDispatch } from 'react-redux';
-// // import { register } from 'redux/auth/operations';
-// import css from './RegisterForm.module.css';
 
-// export const RegisterForm = () => {
-//   // const dispatch = useDispatch();
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-
-//     const name = form.elements.name.value;
-//     const email = form.elements.email.value;
-//     const password = form.elements.password.value;
-
-//     console.log('Name:', name);
-//     console.log('Email:', email);
-//     console.log('Password:', password);
-
-//     // dispatch(
-//     //   register({
-//     //     name: form.elements.name.value,
-//     //     email: form.elements.email.value,
-//     //     password: form.elements.password.value,
-//     //   })
-//     // );
-
-//     form.reset();
-//   };
-
-//   return (
-//     <form className={`${css.form} ${css.blue}`} onSubmit={handleSubmit} autoComplete="off">
-//       <label className={css.label}>
-//         Username
-//         <input type="text" name="name" className={css.input} />
-//       </label>
-//       <label className={css.label}>
-//         Email
-//         <input type="email" name="email" className={css.input} />
-//       </label>
-//       <label className={css.label}>
-//         Password
-//         <input type="password" name="password" className={css.input} />
-//       </label>
-//       <button type="submit" className={css.button}>Register</button>
-//     </form>
-//   );
-// };
 import React, { useState } from 'react';
 // import { useDispatch } from 'react-redux';
-// import { register } from 'redux/auth/operations';
+
 import css from './RegisterForm.module.css';
 import { Link } from 'react-router-dom';
+import { register } from 'redux/auth/operations';
+import { useDispatch } from 'react-redux';
 
 export const RegisterForm = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+//   name: form.elements.name.value,
+//   email: form.elements.email.value,
+//   password: form.elements.password.value,  можно было передавать сразу в dispatch
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -74,17 +32,15 @@ export const RegisterForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log('Name:', name);
-    // console.log('Email:', email);
-    // console.log('Password:', password);
-    console.log('Password, Email, Name:', {name, password, email, });
-    // dispatch(
-    //   register({
-    //     name,
-    //     email,
-    //     password,
-    //   })
-    // );
+   
+    console.log('Password, Email, Name:', {name, password, email });
+    dispatch(
+      register({
+        name,
+        email,
+        password,
+      })
+    );
 
     setName('');
     setEmail('');
