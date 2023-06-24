8b248b9c1ccfd6d04ff000a33e19e2dc96423e9d
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-06-24 16:03:17 +0000

    Deploying to gh-pages from @ IamVovaMoroz/goit-react-hw-08-phonebook@ee88ea45abaa1fa0347cc325ec0d68fbf64687ee 🚀

---
+++ static/js/806.1864d79c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[806],{4806:function(e,o,t){t.r(o);t(2791);var n=t(184);o.default=function(){return(0,n.jsx)("div",{})}}}]);
+//# sourceMappingURL=806.1864d79c.chunk.js.map
