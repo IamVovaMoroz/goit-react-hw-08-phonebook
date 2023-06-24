d79abdb78948a6d1337ae8ac9da1a4ee1c04480e
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-06-24 15:01:47 +0200

    ok

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,14 +6,14 @@ function HomePage() {
   return (
     <div>
         <p >
-        Welcome to the Phone Book application! Try it out today and discover how easy it is to manage your contacts!{' '}
+        Welcome to the Phone Book application! Try it out today and discover how easy it is to manage your contacts!{' '} </p>
        <img
             src={phonebook}
             alt="film Gif"
             style={{ width: '880px', height: '600px' }}
           />
          
-      </p>
+     
     </div>
   )
 }
